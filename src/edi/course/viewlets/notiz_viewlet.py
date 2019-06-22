16c8b9a826e131e730e794f85e90766f0c0dada5
Lars Walther <meinkraeks@gmail.com>
2019-06-22 23:05:48 +0200

    Änderungen für Notizbuch

--- src/edi/course/viewlets/notiz_viewlet.py
+++ src/edi/course/viewlets/notiz_viewlet.py
@@ -3,20 +3,45 @@
 from plone.app.layout.viewlets import ViewletBase
 from zope.component import getUtility
 from plone.i18n.normalizer.interfaces import IIDNormalizer
+from plone import api as ploneapi
+from edi.course.persistance import getCourse
+from Products.CMFPlone.utils import getToolByName
 
 class NotizViewlet(ViewletBase):
 
     def get_action(self):
-        return self.context.absolute_url() + '/validate_notizen'
-   
+        return self.context.absolute_url() + '/validate-notizen'
+
+    def get_homefolder(self):
+        portal = ploneapi.portal.get()
+        membersfolder = portal['Members']
+        pm = getToolByName(portal, 'portal_membership')
+        homeurl = pm.getHomeUrl()
+        folderid = homeurl.split('/')[-1]
+        homefolder = membersfolder[folderid]
+        return homefolder
+
+
+    def get_notizbuch(self, homefolder):
+        kurs = getCourse(self.context)
+        if kurs.id in homefolder.keys():
+            return homefolder[kurs.id]
+        return ''
+
 
     def get_notizen(self):
+        homefolder = self.get_homefolder()
+        notizbuch = self.get_notizbuch(homefolder)
         normalizer = getUtility(IIDNormalizer)
         if hasattr(self.context, 'notizen'):
             if self.context.notizen:
                 notizen = []
                 for i in self.context.notizen:
-                    i['name'] = normalizer.normalize(i['title'])
+                    notizid = normalizer.normalize(i['title'])
+                    i['name'] = notizid
+                    i['data'] = ''
+                    if notizid in notizbuch.keys():
+                        i['data'] = notizbuch[notizid].notiz
                     notizen.append(i)
                 return notizen
         return []
--- src/edi/course/views/validate_notizen.py
+++ src/edi/course/views/validate_notizen.py
@@ -1,11 +1,63 @@
 # -*- coding: utf-8 -*-
-
+import transaction
 from Products.Five.browser import BrowserView
-
+from zope.component import getUtility
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from plone import api as ploneapi
+from Products.CMFCore.utils import getToolByName
+from edi.course.persistance import getCourse
 
 class ValidateNotizen(BrowserView):
+
+    def get_homefolder(self):
+        portal = ploneapi.portal.get()
+        membersfolder = portal['Members']
+        pm = getToolByName(portal, 'portal_membership')
+        homeurl = pm.getHomeUrl()
+        folderid = homeurl.split('/')[-1]
+        homefolder = membersfolder[folderid]
+        return homefolder
+
+
+    def get_notizbuch(self, homefolder):
+        kurs = getCourse(self.context)
+        if not kurs.id in homefolder.keys():
+            notizbuch = ploneapi.content.create(
+                          type = 'Notizbuch',
+                          id = kurs.id,
+                          title = kurs.title,
+                          container=homefolder)
+            transaction.commit()
+        else:
+            notizbuch = homefolder[kurs.id]
+        return notizbuch
+
+
+    def set_notiz(self, notizbuch, notiz):
+        if not notiz['id'] in notizbuch.keys():
+            notiz = ploneapi.content.create(
+                type = 'Notiz',
+                id = notiz['id'],
+                title = notiz['title'],
+                description = notiz['description'],
+                notiz = notiz['data'],
+                link = notiz['url'],
+                container = notizbuch)
+            transaction.commit()
+        else:
+            notizbuch[notiz['id']].notiz = notiz['data']
+
+
     def __call__(self):
-        template = '''<li class="heading" i18n:translate="">
-          Sample View
-        </li>'''
-        return template
+        normalizer = getUtility(IIDNormalizer)
+        homefolder = self.get_homefolder() 
+        if self.request.form:
+            if hasattr(self.context, 'notizen'):
+                if self.context.notizen:
+                    for notiz in self.context.notizen:
+                        notiz['data'] = self.request.get(normalizer.normalize(notiz['title']))
+                        notiz['id'] = normalizer.normalize(notiz['title'])
+                        notiz['url'] = self.context.absolute_url()
+                        if notiz['data']:
+                            notizbuch = self.get_notizbuch(homefolder)
+                            notiz = self.set_notiz(notizbuch, notiz) 
