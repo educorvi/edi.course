121f74b18e1f24b8678e5763d6266ac919801c2d
Lars Walther <meinkraeks@gmail.com>
2019-06-22 13:54:29 +0200

    Viewlet Kursnotiz eingeführt

--- src/edi/course/behaviors/kursnotiz.py
+++ src/edi/course/behaviors/kursnotiz.py
@@ -4,7 +4,10 @@ from edi.course import _
 from plone import schema
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.interfaces import IDexterityContent
+from collective.z3cform.datagridfield import DataGridFieldFactory
+from collective.z3cform.datagridfield import DictRow
 from plone.supermodel import model
+from plone.autoform import directives as form
 from zope.component import adapter
 from zope.interface import Interface
 from zope.interface import implementer
@@ -14,30 +17,28 @@ from zope.interface import provider
 class IKursnotizMarker(Interface):
     pass
 
+class INotizData(model.Schema):
+
+    title = schema.TextLine(
+        title=u'Titel der Notiz im Notizbuch')
+
+    description = schema.Text(
+        title=u'Frage, These oder Aufgabe zu der sich der Lernende eine Notiz machen soll')
+
+
 @provider(IFormFieldProvider)
 class IKursnotiz(model.Schema):
     """
     """
 
-    project = schema.TextLine(
-        title=_(u'Project'),
-        description=_(u'Give in a project name'),
-        required=False,
+    model.fieldset(
+        'notizen',
+        label=u"Notizen zum Inhalt",
+        fields=['notizen',]
     )
 
+    form.widget('notizen', DataGridFieldFactory)
+    notizen = schema.List(title=u'Fragen, Thesen oder Aufgaben für Notizen',
+        description=u'Gib hier Fragen, Thesen oder Aufgaben zu denen sich der Lernende Notizen machen soll.',
+        value_type = DictRow(title=u'Optionen', schema=INotizData))
 
-@implementer(IKursnotiz)
-@adapter(IKursnotizMarker)
-class Kursnotiz(object):
-    def __init__(self, context):
-        self.context = context
-
-    @property
-    def project(self):
-        if hasattr(self.context, 'project'):
-            return self.context.project
-        return None
-
-    @project.setter
-    def project(self, value):
-        self.context.project = value
