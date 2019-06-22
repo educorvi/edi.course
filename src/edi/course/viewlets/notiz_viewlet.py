27f15a4a8e0e1be6fc9cb189917100702c44db9f
Lars Walther <meinkraeks@gmail.com>
2019-06-22 14:27:05 +0200

    Viewlet fertiggestellt

--- src/edi/course/viewlets/notiz_viewlet.py
+++ src/edi/course/viewlets/notiz_viewlet.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
-
+from zope.component import getUtility
+from plone.i18n.normalizer.interfaces import IIDNormalizer
 
 class NotizViewlet(ViewletBase):
 
-    def update(self):
-        self.message = self.get_message()
+    def get_action(self):
+        return self.context.absolute_url() + '/validate_notizen'
+   
 
-    def get_message(self):
-        return u'My message'
+    def get_notizen(self):
+        normalizer = getUtility(IIDNormalizer)
+        if hasattr(self.context, 'notizen'):
+            if self.context.notizen:
+                notizen = []
+                for i in self.context.notizen:
+                    i['name'] = normalizer.normalize(i['title'])
+                    notizen.append(i)
+                return notizen
+        return []
 
     def render(self):
-        return super(NotizViewlet, self).render()
+        if self.get_notizen():
+            return super(NotizViewlet, self).render()
+        return ''
